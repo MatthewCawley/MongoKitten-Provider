7b809ccd0f0179581ae54ae4bc38791a49113960
MatthewCawley <me@matthewcawley.co.uk>
2018-05-07 02:38:54 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "MongoKittenProvider",
-    targets: [
-        Target(name: "MongoKittenProvider", dependencies: ["MongoKitten"])
-        ],
     dependencies: [
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", .branch("master/5.0")),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-    ]
+    ],
+    targets: [
+        Target(name: "MongoKittenProvider", dependencies: ["MongoKitten"])
+        ]
 )
